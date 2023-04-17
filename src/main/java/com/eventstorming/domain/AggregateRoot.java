59652ba75dd1011200cd474d11067a3b40337d4e
kibumpark <top5w5@naver.com>
2023-04-17 16:51:39 +0900

    롤백

--- src/main/java/com/eventstorming/domain/AggregateRoot.java
+++ src/main/java/com/eventstorming/domain/AggregateRoot.java
@@ -54,7 +54,6 @@ public class {{namePascalCase}} {{#checkExtends aggregateRoot.entities.relations
         {{/queryOption.multipleResult}}
         {{/outgoing}}
         **/
-        {{/outgoing}}
     {{/incoming}}
 
         {{namePascalCase}} {{nameCamelCase}} = new {{namePascalCase}}(this);
